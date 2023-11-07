f6046f8391e4fc4d23b2c8bbd3c2d9e1880de2cd
Sidiq Pramono <117287328+sidiqprmn@users.noreply.github.com>
2023-11-07 23:48:34 +0700

    add path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import HomeLogin from "./pages/homeLogin";
+import CardObjek from "./components/card/cardObjek";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={< />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<HomeLogin />} />
+        <Route path="/card" element={<CardObjek />} />
       </Routes>
     </BrowserRouter>
   );
